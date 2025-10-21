7d41b9ae1d6c91417036759e7afb5c6d1e3fa3cf
SadShrimpy <alessionice@gmail.com>
2025-10-21 12:49:19 +0200

    typos in org & title objects

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,21 +1,21 @@
 export const experiences = [
   {
     role: "Sviluppatore Software",
-    org: "Saep Informatica",
+    org: " SRL",
     period: "2023 — Oggi",
     description:
       "Sviluppo, manutenzione e ottimizzazione di applicativi aziendali su piattaforma IBM i, con utilizzo dei linguaggi RPG ILE e FREE. Implementazione di web service per l’integrazione con sistemi esterni, con gestione dello scambio dati in formato JSON e XML.",
   },
   {
     role: "Sviluppatore Web Freelance",
-    org: "Cristal Domino",
+    org: "Cristal Domino SAS",
     period: "06/2022 - 11/2022",
     description:
       "Sviluppo e manutenzione del sito web cristaldomino.com.",
   },
   {
     role: "Sviluppatore Full Stack Freelance",
-    org: "Gru Master",
+    org: "Gru Master SAGL",
     period: "06/2022 - 11/2022",
     description:
       "Sviluppo e manutenzione sito web grumaster.ch utilizzando JavaScript, MySQL e PHP. Gestione e monitoraggio dei servizi tramite applicazioni mobile.",
