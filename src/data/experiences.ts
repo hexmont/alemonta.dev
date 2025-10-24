102929117b9240fa689b6fedba374e151818fb06
SadShrimpy <alessionice@gmail.com>
2025-10-24 08:57:35 +0200

    fix: correct organization names and update employment periods in experiences data

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,8 +1,8 @@
 export const experiences = [
   {
     role: "Sviluppatore Software",
-    org: " SRL",
-    period: "2023 — Oggi",
+    org: "Saep Informatica SRL",
+    period: "2023 — OGGI",
     description:
       "Sviluppo, manutenzione e ottimizzazione di applicativi aziendali su piattaforma IBM i, con utilizzo dei linguaggi RPG ILE e FREE. Implementazione di web service per l’integrazione con sistemi esterni, con gestione dello scambio dati in formato JSON e XML.",
   },
@@ -16,7 +16,7 @@ export const experiences = [
   {
     role: "Sviluppatore Full Stack Freelance",
     org: "Gru Master SAGL",
-    period: "06/2022 - 11/2022",
+    period: "06/2025 - OGGI",
     description:
       "Sviluppo e manutenzione sito web grumaster.ch utilizzando JavaScript, MySQL e PHP. Gestione e monitoraggio dei servizi tramite applicazioni mobile.",
   },
