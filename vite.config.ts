dd84c4aae4698fced7d24e8c57044a07fad5b59e
SadShrimpy <alessionice@gmail.com>
2025-10-27 09:56:42 +0100

    add vite-plugin-sitemap for automatic sitemap generation and robots.txt support

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss()
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://alemonta.dev',
+      generateRobotsTxt: true,
+      // dynamicRoutes: ['/projects', '/about'], // todo: implement dynamic routes if needed
+      // exclude: ['/private'], // todo: exclude private routes if needed
+    })
   ],
   resolve: {
     alias: {
